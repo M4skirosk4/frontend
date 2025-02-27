88b337283bc0ca38b9266f36f8d8d69e612dac70
M4skirovk4 <119711259+M4skirosk4@users.noreply.github.com>
2025-02-27 14:10:41 +0100

    First version
    In this first iteration, the frontend is able to receive the message from the microservices

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Microservicios con Vue</h1>
+      <button @click="consultarMicroservicio1">Microservicio 1</button>
+      <input type="text" v-model="mensaje1" readonly />
+  
+      <button @click="consultarMicroservicio2">Microservicio 2</button>
+      <input type="text" v-model="mensaje2" readonly />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        mensaje1: "",
+        mensaje2: "",
+      };
+    },
+    methods: {
+      async consultarMicroservicio1() {
+        const response = await fetch("http://localhost:8081/api/mensaje1");
+        this.mensaje1 = await response.text();
+      },
+      async consultarMicroservicio2() {
+        const response = await fetch("http://localhost:8082/api/mensaje2");
+        this.mensaje2 = await response.text();
+      },
+    },
+  };
+  </script>
+  
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+      <button @click="consultarMicroservicio1">Obtener mensaje de Microservicio 1</button>
+      <button @click="consultarMicroservicio2">Obtener mensaje de Microservicio 2</button>
+      <p>{{ response }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        response: ''
+      }
+    },
+    methods: {
+      consultarMicroservicio1() {
+        fetch('http://localhost:8081/api/mensaje1')
+          .then(response => response.text())
+          .then(data => {
+            this.response = data
+          })
+          .catch(error => {
+            console.error("Error al obtener mensaje de Microservicio 1:", error);
+            this.response = 'Error al obtener mensaje de Microservicio 1';
+          });
+      },
+      consultarMicroservicio2() {
+        fetch('http://localhost:8082/api/mensaje2')
+          .then(response => response.text())
+          .then(data => {
+            this.response = data
+          })
+          .catch(error => {
+            console.error("Error al obtener mensaje de Microservicio 2:", error);
+            this.response = 'Error al obtener mensaje de Microservicio 2';
+          });
+      }
+    }
+  }
+  </script>
